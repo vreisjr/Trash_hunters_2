626cc69f60fdf649820acc53b70bab923fb9eb66
EmanoelLuiz01 <es046382@gmail.com>
2026-09-11 21:23:29 -0300

    alteração no css

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(CategoriaSeeder::class);
+
         // User::factory(10)->create();
 
         User::factory()->create([
